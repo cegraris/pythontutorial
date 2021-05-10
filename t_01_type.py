72d75114d51faa6dc8a875e50c4a82ef435f886b
cegraris <wusthink@gmail.com>
2021-05-10 08:29:54 +0200

    init

---
+++ t_01_type.py
@@ -0,0 +1,49 @@
+import math
+import random
+# number======================================================
+# print(type(1234),type(3.1415),type(3+4j))
+# print(123+222)
+# print(1.5*4)
+# print(2**100)
+# print(math.pi)
+# print(math.sqrt(85))
+# print(random.random())
+# print(random.choice([1,2,3,4]))
+
+# string =====================================================
+# S = 'Spam'
+# print(type(S))
+# print(len(S))
+# print(S[1])
+# print(S[-1])
+# print(S[1:3])
+# print(S[1:])
+# print(S[:-1])
+# print(S[:])
+# print(S+'xyz')
+# print(S*8)
+# print(S.find('pa'))
+# print(S.replace('pa','XYZ'))
+# line = 'aaa,bbb,ccccc,dd'
+# print(line.split(','))
+# print(S.upper())
+# print(S.isalpha())
+# print(S.isdigit())
+# line = 'aaa,bbb,ccccc,dd\n'
+# print(line.rstrip())
+# print('---')
+# print('%s, eggs, and %s'%('spam','SPAM!') )
+# print('{0}, eggs,and {1}'.format('spam','SPAM!'))
+# print(dir(S))
+# print(help(S.replace))
+# print(help(str))
+# S = 'A\nB\tC'
+# print(S)
+# print(len(S))
+# print(ord('\n')) # ASCII
+# print('A\0B\0C')
+# print("""aaaaa
+# bbb'''bbbbb""bbbbbb'bbbb
+# CCCCCCCCC""")
+
+#
